788525c0a1255d6321214aba50f493dd9ad2f8bf
Dulieka Himashan <d.shanz0805@gmail.com>
2026-03-25 16:30:19 +0530

    Update API URL to cloud backend

--- mobile/parking_app/lib/services/api_service.dart
+++ mobile/parking_app/lib/services/api_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = 'http://10.0.2.2:5001/api';
+  static const String baseUrl = 'https://cgp-mobile-app.onrender.com/api';
 
   // Get parking slots
   static Future<Map<String, dynamic>> getSlots() async {
